d82ac5c9cc126dc16d1410ab0908d7497eee4522
InvaderJT <invaderjt@gmail.com>
2026-05-25 21:17:08 -0400

    getting started

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from textnode import *
+
+
+
+
+
+def main():
+    dummy = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(dummy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
